c04dde24a81d67bbc79168d48d5055f8a5174c18
hardikshoppiko <112391316+hardikshoppiko@users.noreply.github.com>
2026-03-24 20:22:25 +0530

    Update Login Page

--- src/app/account/login/page.js
+++ src/app/account/login/page.js
@@ -1,3 +1,6 @@
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 import { Suspense } from "react";
 import LoginForm from "./LoginForm";
 import styles from "./page.module.css";
